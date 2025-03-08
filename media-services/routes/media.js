d2f50574d11f43b1e74ac9eda85068a5d18f4f4d
Sulthon🔥 <sulthonhere@gmail.com>
2025-03-08 23:05:02 +0700

    add: multiple image operation API
    - Update Create API
    - Add Get API
    - Add Delete API

--- media-services/routes/media.js
+++ media-services/routes/media.js
@@ -3,8 +3,28 @@ const router = express.Router();
 
 const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
+const fs = require('fs');
+
 const { Media } = require('../models');
 
+// ===Get Function===
+router.get('/', async (req, res) => {
+    const media = await Media.findAll({
+      attributes: ['id', 'image']
+    });
+
+    const mappedMedia = media.map( (m) => {
+      m.image = `${req.get('host')}/${m.image}`;
+      return m;
+    });
+
+    return res.json({
+      status: 'success',
+      data: mappedMedia
+    });
+});
+
+// ===Post Function===
 router.post('/', (req, res) => {
   const image = req.body.image;
 
@@ -18,7 +38,7 @@ router.post('/', (req, res) => {
     }
 
     const filename = filepath.split('\\').pop().split('/').pop();
-    const media = await Media.create({ image: `image/${filename}` });
+    const media = await Media.create({ image: `images/${filename}` });
 
     return res.json({
       status: 'success',
@@ -28,6 +48,31 @@ router.post('/', (req, res) => {
       }
     });
 
+  });
+
+});
+
+// ===Delete Function===
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  const media = await Media.findByPk(id);
+
+  if (!media){
+    return res.status(404).json({ status: 'error', message: 'media not found!'});
+  }
+
+  fs.unlink(`./public/${media.image}`, async (err) => {
+    if (err) {
+      return res.status(400).json({ status: 'error', message: err.message });
+    }
+
+    await media.destroy();
+
+    return res.json({
+      status: 'success',
+      message: 'image deleted!'
+    })
   });
 
 });
