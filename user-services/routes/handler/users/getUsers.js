562019734c21de5b5ad7bb2bb4b975e9f364c08f
Sulthon🔥 <sulthonhere@gmail.com>
2025-03-14 17:40:07 +0700

    add: multiple users endpoint

---
+++ user-services/routes/handler/users/getUsers.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt');
+const Validator = require('fastest-validator');
+const v = new Validator();
+
+const { User } = require('../../../models');
+
+module.exports = async (req, res) => {
+    const userIds = req.query.user_ids || [];
+
+    const sqlOptions = {
+        attributes: ['id', 'name', 'email', 'role', 'profession', 'avatar']
+    }
+
+    if (userIds.length) {
+        sqlOptions.where = {
+            id: userIds
+        }
+    }
+
+    const users = await User.findAll(sqlOptions);
+
+    return res.json({
+        status: 'success',
+        data: users
+    });
+}
--- user-services/routes/users.js
+++ user-services/routes/users.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const usersHandler = require('./handler/users');
 
+router.get('/:id', usersHandler.getUser);
+router.get('/', usersHandler.getUsers);
+
 router.post('/register', usersHandler.register);
+router.post('/login', usersHandler.login);
+
+router.put('/:id', usersHandler.update);
 
 module.exports = router;
